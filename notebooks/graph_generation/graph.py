a994766c90178d720bb25816ab64e70ea9eceede
Javier Duarte <jduarte1@fnal.gov>
2019-03-13 03:09:14 +0000

    update graph

--- notebooks/graph_generation/graph.py
+++ notebooks/graph_generation/graph.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections import namedtuple
 
 SparseGraph = namedtuple('SparseGraph',
-        ['X', 'Ri_rows', 'Ri_cols', 'Ro_rows', 'Ro_cols', 'y','simmatched'])
+        ['X', 'Ri_rows', 'Ri_cols', 'Ro_rows', 'Ro_cols', 'y', 'simmatched'])
 
 def make_sparse_graph(X, Ri, Ro, y):
     Ri_rows, Ri_cols = Ri.nonzero()
@@ -25,3 +25,12 @@ def load_graph(filename, graph_type=SparseGraph):
 
 def load_graphs(filenames, graph_type=SparseGraph):
     return [load_graph(f, graph_type) for f in filenames]
+
+def graph_from_sparse(sparse_graph, dtype=np.uint8):
+    n_nodes = sparse_graph.X.shape[0]
+    n_edges = sparse_graph.Ri_rows.shape[0]
+    Ri = np.zeros((n_nodes, n_edges), dtype=dtype)
+    Ro = np.zeros((n_nodes, n_edges), dtype=dtype)
+    Ri[sparse_graph.Ri_rows, sparse_graph.Ri_cols] = 1
+    Ro[sparse_graph.Ro_rows, sparse_graph.Ro_cols] = 1
+    return Graph(sparse_graph.X, Ri, Ro, sparse_graph.y)
