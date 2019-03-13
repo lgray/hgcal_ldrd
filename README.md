01d98ee02d25f200e6fa05e1fb807c04ae52dd8a
Lindsey Gray <Lindsey.Gray@cern.ch>
2019-03-13 17:07:48 -0500

    use correct adj. matrix representation, use sparse matrices, add progress bar :-)

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ conda create --name hgcal-env python=3.6
 source activate hgcal-env
 conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
 conda install pandas matplotlib jupyter nbconvert==5.4.1
+conda install -c conda-forge tqdm
 pip install uproot scipy sklearn --user
 ```
--- notebooks/graph_generation/graph.py
+++ notebooks/graph_generation/graph.py
@@ -1,13 +1,16 @@
 import numpy as np
 from collections import namedtuple
+from scipy.sparse import csr_matrix
+
+Graph = namedtuple('Graph', ['X', 'Ri', 'Ro', 'y','simmatched'])
 
 SparseGraph = namedtuple('SparseGraph',
         ['X', 'Ri_rows', 'Ri_cols', 'Ro_rows', 'Ro_cols', 'y', 'simmatched'])
 
-def make_sparse_graph(X, Ri, Ro, y):
+def make_sparse_graph(X, Ri, Ro, y,simmatched=None):
     Ri_rows, Ri_cols = Ri.nonzero()
     Ro_rows, Ro_cols = Ro.nonzero()
-    return SparseGraph(X, Ri_rows, Ri_cols, Ro_rows, Ro_cols, y)
+    return SparseGraph(X, Ri_rows, Ri_cols, Ro_rows, Ro_cols, y, simmatched)
 
 def save_graph(graph, filename):
     """Write a single graph to an NPZ file archive"""
@@ -29,8 +32,8 @@ def load_graphs(filenames, graph_type=SparseGraph):
 def graph_from_sparse(sparse_graph, dtype=np.uint8):
     n_nodes = sparse_graph.X.shape[0]
     n_edges = sparse_graph.Ri_rows.shape[0]
-    Ri = np.zeros((n_nodes, n_edges), dtype=dtype)
-    Ro = np.zeros((n_nodes, n_edges), dtype=dtype)
-    Ri[sparse_graph.Ri_rows, sparse_graph.Ri_cols] = 1
-    Ro[sparse_graph.Ro_rows, sparse_graph.Ro_cols] = 1
-    return Graph(sparse_graph.X, Ri, Ro, sparse_graph.y)
+    mat_shape = (n_nodes,n_edges)
+    data = np.ones(n_edges)
+    Ri = csr_matrix((data,(sparse_graph.Ri_rows,sparse_graph.Ri_cols)),mat_shape,dtype=dtype)
+    Ro = csr_matrix((data,(sparse_graph.Ro_rows,sparse_graph.Ro_cols)),mat_shape,dtype=dtype)
+    return Graph(sparse_graph.X, Ri, Ro, sparse_graph.y, sparse_graph.simmatched)
