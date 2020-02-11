f823680436b00aa1dcee484108f73e9314fa8726
Lindsey Gray <lindsey.gray@gmail.com>
2020-02-11 21:07:33 +0000

    new graph regression / categorization model, reasonable performance on NMIST

--- src/models/DynamicReductionNetwork.py
+++ src/models/DynamicReductionNetwork.py
@@ -12,8 +12,22 @@ import torch_geometric.transforms as T
 from torch.utils.checkpoint import checkpoint
 from torch_cluster import knn_graph
 
-from torch_geometric.nn import EdgeConv
+from torch_geometric.nn import EdgeConv, NNConv
 from torch_geometric.nn.pool.edge_pool import EdgePooling
+
+from torch_geometric.utils import normalized_cut
+from torch_geometric.utils import remove_self_loops
+from torch_geometric.utils.undirected import to_undirected
+from torch_geometric.nn import (graclus, max_pool, max_pool_x,
+                                global_mean_pool, global_max_pool,
+                                global_add_pool)
+
+transform = T.Cartesian(cat=False)
+
+def normalized_cut_2d(edge_index, pos):
+    row, col = edge_index
+    edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
+    return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
 
 class DynamicReductionNetwork(nn.Module):
     # This model iteratively contracts nearest neighbour graphs 
@@ -23,60 +37,60 @@ class DynamicReductionNetwork(nn.Module):
     # This allows single quantities to be regressed from complex point counts
     # in a location and orientation invariant way.
     # One encoding layer is used to abstract away the input features.
-    def __init__(self, input_dim=5, hidden_dim=128, output_dim=1, k=8, aggr='add',
+    def __init__(self, input_dim=5, hidden_dim=128, output_dim=1, k=8, n_iters = 4, aggr='add',
                  norm=torch.tensor([1./500., 1./500., 1./54., 1/25., 1./1000.])):
         super(DynamicReductionNetwork, self).__init__()
 
         self.datanorm = nn.Parameter(norm)
         
         self.k = k
-        start_width = 2 * hidden_dim
-        middle_width = 3 * hidden_dim // 2
+        start_width = 4 * hidden_dim
+        middle_width = 3 * hidden_dim
 
         self.inputnet =  nn.Sequential(
             nn.Linear(input_dim, 2*hidden_dim),            
-            nn.Tanh(),
+            nn.ELU(),
             nn.Linear(2*hidden_dim, 2*hidden_dim),
-            nn.Tanh(),
-            nn.Linear(2*hidden_dim, hidden_dim),
-            nn.Tanh(),
+            nn.ELU(),
+            nn.Linear(2*hidden_dim, 2*hidden_dim),
+            nn.ELU(),
         )        
         convnn = nn.Sequential(nn.Linear(start_width, middle_width),
-                               nn.ReLU(),
-                               nn.Linear(middle_width, hidden_dim),                                             
-                               nn.ReLU()
+                               nn.ELU(),
+                               nn.Linear(middle_width, 2*hidden_dim),                                             
+                               nn.ELU()
                                )
-        
+                
         self.edgeconv = EdgeConv(nn=convnn, aggr=aggr)
         
         self.reducer = EdgePooling(hidden_dim)
         
-        self.output = nn.Sequential(nn.Linear(hidden_dim, 2*hidden_dim),
-                                    nn.ReLU(),
-                                    nn.Linear(2*hidden_dim, 2*hidden_dim),
-                                    nn.ReLU(),
-                                    nn.Linear(2*hidden_dim, hidden_dim),
-                                    nn.ReLU(),
+        self.output = nn.Sequential(nn.Linear(2*hidden_dim, hidden_dim),
+                                    nn.ELU(),
+                                    nn.Dropout(),
                                     nn.Linear(hidden_dim, hidden_dim//2),
-                                    nn.ReLU(),
+                                    nn.ELU(),                                    
                                     nn.Linear(hidden_dim//2, output_dim))
         
         
-    def forward(self, data, batch=None):        
-        x_norm = self.datanorm * data.x
-        H = self.inputnet(x_norm)
-        n_batches = torch.unique(batch)
-        out = None
-        for i in n_batches:
-            batch_mask = (batch == i)
-            H_i = H[batch_mask]
-            ibatch = torch.zeros(H_i.shape[0], dtype=torch.int64).to(H_i.device)
-            while H_i.shape[0] > 1:
-                edge_index = knn_graph(H_i, self.k, ibatch, loop=False, flow=self.edgeconv.flow)
-                Hprime = self.edgeconv(H_i, edge_index)
-                H_i, _, ibatch, _ = self.reducer(Hprime, edge_index, ibatch)
-            if out is None:
-                out = H_i
-            else:
-                out = torch.cat([out, H_i], dim=0)
-        return self.output(out).squeeze(-1)
+    def forward(self, data):        
+        data.x = self.datanorm * data.x
+        data.x = self.inputnet(data.x)        
+        data.edge_index = knn_graph(data.pos, self.k, data.batch, loop=False, flow=self.edgeconv.flow)
+                
+        data.x = self.edgeconv(data.x, data.edge_index)
+        
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        data.edge_attr = None
+        data = max_pool(cluster, data)
+        
+        data.x = self.edgeconv(data.x, data.edge_index)
+        
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        x, batch = max_pool_x(cluster, data.x, data.batch)
+
+        x = global_max_pool(x, batch)
+        
+        return self.output(x).squeeze(-1)
